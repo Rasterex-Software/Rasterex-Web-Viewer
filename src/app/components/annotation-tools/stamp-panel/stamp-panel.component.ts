89db7cf94254e52e65a2c635f09f2253af139dd9
Jawad Sharif <jawad.bstreamssolutions@gmail.com>
2024-06-12 01:50:04 +0500

    WIP Custom Stamps

--- src/app/components/annotation-tools/stamp-panel/stamp-panel.component.ts
+++ src/app/components/annotation-tools/stamp-panel/stamp-panel.component.ts
@@ -11,12 +11,44 @@ export class StampPanelComponent implements OnInit {
   @Output() onClose: EventEmitter<void> = new EventEmitter<void>();
 
   templates: any = STAMP_TEMPLATES;
+  customTemplates: any = [];
   opened: boolean = false;
   activeIndex: number = 0;
+  height: number | undefined = 20;
+  width: number | undefined= 20;
+  previewSrc = ''
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
+    this.previewSrc = '';
+    var templatesString = localStorage.getItem('customStamp') || '[]';
+    this.customTemplates = JSON.parse(templatesString);
+  }
+
+  handleImage(event) {
+    const file = event?.target?.files[0];
+    if (!file) {
+      return;
+    }
+    // Create a blob URL from the file
+    const blobUrl = URL.createObjectURL(file);
+    this.previewSrc = blobUrl
+
+  }
+  addCustomStamp(){
+    if(!this.previewSrc) {
+      this.opened  = false;
+    }
+    console.log('Stamp saved. URL:', this.previewSrc);
+    this.customTemplates.push({
+      id: 10,
+      src: this.previewSrc,
+      height: this.height,
+      width: this.width
+    })
+    this.opened = false;
+    localStorage.setItem('customStamp', JSON.stringify(this.customTemplates || []));
   }
 
   onPanelClose(): void {

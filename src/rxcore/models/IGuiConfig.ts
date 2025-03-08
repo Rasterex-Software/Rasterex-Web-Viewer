f1b1bd1b6a05fa857cc9215593ce23f3f753cca7
egghunters <egghunters@163.com>
2025-03-08 08:55:09 +0800

    Implement storing annotations into db

--- src/rxcore/models/IGuiConfig.ts
+++ src/rxcore/models/IGuiConfig.ts
@@ -73,6 +73,7 @@ export interface IGuiConfig {
     disableMonochromeButton?: boolean;
     enableGrayscaleButton?: boolean;
     localStoreStamp?: boolean;
+    localStoreAnnotation?: boolean;
     /*  */
     logoUrl?: string;
 }

b94d9d9b8f3f1f57c307a735676b1754d61bc2bf
Abuzar <hmabozar@gmail.com>
2025-05-15 17:09:53 +0500

    Incorporated feedback on inconsistent sidebar button behavior.

--- src/app/components/side-nav-menu/side-nav-menu.component.ts
+++ src/app/components/side-nav-menu/side-nav-menu.component.ts
@@ -23,23 +23,41 @@ export class SideNavMenuComponent implements OnInit {
   numpages: number;
   canChangeSign: boolean = false;
 
-  
+  guiConfig: any;
+  guiState: any;
+  guiMode: any;
+  containLayers: boolean = false;
+  containBlocks: boolean = false;
 
   ngOnInit(): void {
     this.guiState$.subscribe(state => {
       this.numpages = state.numpages;
+      this.guiState = state;
       //this.toggleablePanelOpened = false;
       //this.activeIndex = -1;
       this.canChangeSign = state.numpages && state.isPDF && RXCore.getCanChangeSign();
     });
 
+    this.rxCoreService.guiVectorLayers$.subscribe((layers) => {
+      this.containLayers = layers.length > 0;
+    });
+
+    this.rxCoreService.guiVectorBlocks$.subscribe((blocks) => {
+      this.containBlocks = blocks.length > 0;
+    });
+
     this.guiMode$.subscribe((mode: GuiMode) => {
+      this.guiMode = mode;
       if (mode == GuiMode.Signature) {
         this.toggle(5);
       } else {
         this.toggleablePanelOpened = false;
 
       }
+    });
+
+    this.guiConfig$.subscribe((config) => {
+      this.guiConfig = config;
     });
 
     this.sideNavMenuService.sidebarChanged$.subscribe((index: number) => {
@@ -58,6 +76,44 @@ export class SideNavMenuComponent implements OnInit {
     const openIndex = [0, 3, 4, 5, 6].includes(index);
     this.toggleablePanelOpened = openIndex ? this.activeIndex !== index || !this.toggleablePanelOpened : false;
     this.activeIndex = !this.toggleablePanelOpened && openIndex ? -1 : index;
-    
+
+  }
+
+  visibleSidebarItemsCount(): number {
+    const visibleItems = this.getVisibleSidebarItems();
+    return visibleItems.length;
+  }
+
+  private getVisibleSidebarItems() {
+    const visibleItems = [
+      { index: 0, visible: !this.guiConfig?.disableViewPages },
+      {
+        index: 5,
+        visible:
+          this.guiConfig?.canSignature &&
+          this.canChangeSign &&
+          this.guiMode == GuiMode.Signature,
+      },
+      {
+        index: 3,
+        visible:
+          !this.guiConfig?.disableViewVectorLayers &&
+          (this.guiState?.is2D || this.guiState?.isPDF) &&
+          this.containLayers,
+      },
+      {
+        index: 6,
+        visible:
+          !this.guiConfig?.disableViewVectorLayers &&
+          this.guiState?.is2D &&
+          this.containBlocks,
+      },
+      {
+        index: 4,
+        visible: !this.guiConfig?.disableView3DParts && this.guiState?.is3D,
+      },
+    ];
+
+    return visibleItems.filter((item) => item.visible);
   }
 }

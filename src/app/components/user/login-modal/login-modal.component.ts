94e758fdfa3bc4be7c42366ed08a7370c57217ad
Rohit <rohit.choudharytechy@gmail.com>
2025-05-16 12:19:05 +0530

    Implemented login UI and logged-in user profile  improvements and simplified "file open" option from bruger menu

---
+++ src/app/components/user/login-modal/login-modal.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoginService } from 'src/app/services/login.service';
+import { UserService, User } from '../user.service';
+import { RXCore } from 'src/rxcore';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss'],
+})
+export class LoginModalComponent implements OnInit, OnDestroy {
+  loginUsername = '';
+  loginPassword = '';
+  useBuildinUser = true;
+  selectedBuildinUsername = 'bob';
+  isLoggingIn = false;
+  isLoginFailed = false;
+
+  private loginModalSub: Subscription | undefined;
+
+  constructor(
+    private loginService: LoginService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.loginModalSub = this.loginService.loginModalVisible$.subscribe((visible) => {
+      if (visible) {
+        this.resetForm();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.loginModalSub?.unsubscribe();
+  }
+
+  get visible() {
+    return this.loginService.loginModalVisible$.value;
+  }
+
+  resetForm() {
+    this.useBuildinUser = true;
+    this.selectedBuildinUsername = 'bob';
+    this.loginUsername = this.selectedBuildinUsername;
+    this.loginPassword = '123456';
+    this.isLoginFailed = false;
+    this.isLoggingIn = false;
+  }
+
+  close() {
+    this.loginService.hideLoginModal();
+    this.isLoggingIn = false;
+  }
+
+  onBuildinUsernameChange() {
+    if (this.selectedBuildinUsername === '') {
+      this.useBuildinUser = false;
+      this.loginUsername = '';
+    } else {
+      this.useBuildinUser = true;
+      this.loginUsername = this.selectedBuildinUsername;
+      this.loginPassword = '123456';
+    }
+  }
+
+  onLogin() {
+    this.isLoginFailed = false;
+    this.isLoggingIn = true;
+    this.userService.login(this.loginUsername, this.loginPassword)
+      .then((user: User) => {
+
+       try {
+          RXCore.setUser(user.username, user.displayName || user.username);
+        } catch (err) {
+          console.log(err);
+        }  
+
+        this.userService.getPermissions(1, user.id).then(res => {
+          this.userService.setUserPermissions(res);
+          this.loginService.setPermissionsInProfilePanel(res);
+        });
+        this.loginService.hideLoginModal();
+        this.loginService.setLoginInfo(user.username, user.displayName || user.username, user.email);
+      })
+      .catch((e) => {
+        console.error('Login failed:', e.error || e.message);
+        this.isLoginFailed = true;
+        // alert(e.error?.message || 'Login failed');
+      })
+      .finally(() => {
+        this.isLoggingIn = false;
+      });
+  }
+}
--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { RxCoreService } from 'src/app/services/rxcore.service';
 import { RXCore } from 'src/rxcore';
 import { User, UserService } from '../user.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'rx-login',
@@ -9,100 +10,57 @@ import { User, UserService } from '../user.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  guiMode$ = this.rxCoreService.guiMode$;
   username = '';
   displayName = '';
   email = '';
-  permissions = '';
+  groupedPermissions: { [type: string]: string[] } = {};
   isLoggingIn = false;
   isLoggingOut = false;
-  isLoginFailed = false;
-  loginPanelOpened = false;
   userInfoPanelOpened = false;
-  loginUsername = '';
-  loginPassword = '';
-
-  useBuildinUser: boolean;
-  selectedBuildinUsername: string = '';
+  objectKeys = Object.keys;
 
 
-  constructor (
+     @ViewChild('userInfoPanel', { static: false }) userInfoPanelRef!: ElementRef;
+
+  constructor(
     private readonly rxCoreService: RxCoreService,
     private readonly userService: UserService,
+    public loginService: LoginService
   ) {}
 
   ngOnInit(): void {
     this.rxCoreService.guiState$.subscribe((state) => {
+
+       this.loginService.username$.subscribe(username => {
+    this.username = username;
+  });
+
+  this.loginService.displayName$.subscribe(name => {
+    this.displayName = name;
+  });
+
+ this.loginService.email$.subscribe(name => {
+    this.email = name;
+  });
+
+   this.loginService.permissions$.subscribe(permission => {
+    this.groupedPermissions = permission;
+  });
     });
   }
 
   openLoginDialog() {
-    this.loginPanelOpened = true;
-    // Use buildin user by default, and set 'bob' the selected option, and, fill in password
-    this.useBuildinUser = true;
-    this.selectedBuildinUsername = 'bob';
-    this.loginUsername = this.selectedBuildinUsername;
-    this.loginPassword = '123456';
-    
+    this.loginService.showLoginModal();
   }
 
-  closeLoginDialog() {
-    this.loginPanelOpened = false;
-    this.isLoggingIn = false;
-  }
-
-  toggleUserInfoPanel() {
-    this.userInfoPanelOpened = !this.userInfoPanelOpened;
-  }
-
-  closeUserInfoPanel() {
-    this.userInfoPanelOpened = false;
-  }
-
-  onLogin() {
-    this.isLoggingIn = true;
-    this.userService.login(this.loginUsername, this.loginPassword)
-      .then((user: User) => {
-        this.username = this.loginUsername;
-        this.displayName = user.displayName || '';
-        this.email = user.email;
-        this.loginPanelOpened = false;
-        this.closeLoginDialog();
-
-        try {
-          RXCore.setUser(user.username, user.displayName || user.username);
-        } catch (err) {
-          console.log(err);
-        }
-
-        console.log('Login success:', user);
-        // TODO: hard code projId to 1
-
-
-        this.userService.getPermissions(1, user.id).then(res => {
-          this.userService.setUserPermissions(res);
-          if (Array.isArray(res)) {
-            const permKeys = res.map((item) => item.permission.key).join(', ');
-            this.permissions = permKeys;
-          }
-        });
-        /*this.userService.getAnnotations(1).then(res => {
-          this.userService.setAnnotations(res);
-        });*/
-      }).catch((e) => {
-        console.error('Login failed:', e.error || e.message);
-        alert(e.error.message);
-      }).finally(() => {
-        this.isLoggingIn = false;
-      });
-  }
-
+ 
   onLogoutClick() {
     this.isLoggingOut = true;
     this.userService.logout()
       .then(() => {
         this.userService.setUserPermissions(); // clear permissions
         this.username = '';
+        this.displayName = '';
         this.email = '';
         RXCore.setUser('', '');
       })
@@ -112,19 +70,21 @@ export class LoginComponent implements OnInit {
       .finally(() => {
         this.isLoggingOut = false;
         this.userInfoPanelOpened = false;
+        this.loginService.clearLoginInfo();
       });
   }
 
-  onBuildinUsernameChange() {
-    if (this.selectedBuildinUsername === '') {
-      this.useBuildinUser = false;
-      this.loginUsername = '';
-    } else {
-      this.useBuildinUser = true;
-      this.loginUsername = this.selectedBuildinUsername;
-      this.loginPassword = '123456';
-    }
+  toggleUserInfoPanel() {
+    this.userInfoPanelOpened = !this.userInfoPanelOpened;
   }
 
-
+@HostListener('document:click', ['$event'])
+onClickOutside(event: MouseEvent) {
+  const target = event.target as HTMLElement;
+  const clickedInsidePanel = this.userInfoPanelRef?.nativeElement.contains(target);
+  const clickedUserButton = target.closest('#userInfoButton');
+  if (!clickedInsidePanel && !clickedUserButton && this.userInfoPanelOpened) {
+    this.userInfoPanelOpened = false;
+  }
+}
 }
